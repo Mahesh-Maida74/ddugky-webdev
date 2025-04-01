5ea7d801e8e63351f111ef3f0f532773b14f3ccf
mahesh.maida.7489 <mahesh.maida.7489@gmail.com>
2025-04-01 11:12:45 +0530

    DataTypes and Variables

--- JS-Fundamentals/index.js
+++ JS-Fundamentals/index.js
@@ -6,3 +6,38 @@ console.log(name);
 let a = 10;
 let b = 20;
 console.log(a+b);
+
+//Data Type **********
+// ******String Type Data***********
+fName = "Mahesh";
+lName = "Maida";
+console.log(fName+lName);
+
+//******Number Type Data */
+num1 = 20;
+num2 = 30;
+console.log(num1+num2);
+
+
+//******Boolean Data Type */
+age = 18;
+console.log(age<16);
+
+//****Undefined*** */
+nam = "";
+//*****Null */
+
+
+//*****Symbol Data Type */
+
+
+//*******Variables in js */
+var fName = "Mahesh";
+var lName = "Maida";
+var age = 21;
+
+var sname = null;
+
+
+console.log("My Name is ",fName+lName,"!");
+console.log("My Name is ",fName+lName,"and my age is ",age,".");
